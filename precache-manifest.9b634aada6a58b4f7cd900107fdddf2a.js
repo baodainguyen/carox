bee0587190ebaccc595e369e612f55072cc83ae3
Nguyen Bao Dai <dainb@Dais-McMini.local>
2020-01-18 10:34:30 +0700

    Updates

---
+++ precache-manifest.9b634aada6a58b4f7cd900107fdddf2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6f9b934cd9c489dc5563d63d2b6a89",
+    "url": "/carox/index.html"
+  },
+  {
+    "revision": "65c90f61779788d554fe",
+    "url": "/carox/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d49bc573c652723be216",
+    "url": "/carox/static/js/2.6360ac69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/carox/static/js/2.6360ac69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65c90f61779788d554fe",
+    "url": "/carox/static/js/main.47f60ea3.chunk.js"
+  },
+  {
+    "revision": "10755e6d01a3a1236a3b",
+    "url": "/carox/static/js/runtime-main.1be5d244.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carox/static/media/logo.5d5d9eef.svg"
+  }
+]);
