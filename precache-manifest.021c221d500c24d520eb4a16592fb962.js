2a74bb3985cf3607272849c4efbbfd36e14de797
Nguyen Bao Dai <dainb@Dais-McMini.local>
2020-01-22 00:12:40 +0700

    Updates

--- precache-manifest.021c221d500c24d520eb4a16592fb962.js
+++ precache-manifest.021c221d500c24d520eb4a16592fb962.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2dd91f951617ac13df71a7b61ae124e9",
+    "revision": "6b96d3e2df4c1c7f9ff3b5c5b02b7fbc",
     "url": "/carox/index.html"
   },
   {
-    "revision": "8ae7137b0b40569bfc4f",
+    "revision": "cf270e87febe5e1ca99d",
     "url": "/carox/static/css/main.6bf5d6c1.chunk.css"
   },
   {
-    "revision": "2f0413b3e7ab2307ab2d",
-    "url": "/carox/static/js/2.54370830.chunk.js"
+    "revision": "b70490f2d1d005dcbf77",
+    "url": "/carox/static/js/2.5a9b8ffd.chunk.js"
   },
   {
     "revision": "8de4bdbb127f280cc6b79279dd69f244",
-    "url": "/carox/static/js/2.54370830.chunk.js.LICENSE"
+    "url": "/carox/static/js/2.5a9b8ffd.chunk.js.LICENSE"
   },
   {
-    "revision": "8ae7137b0b40569bfc4f",
-    "url": "/carox/static/js/main.47f0e9c7.chunk.js"
+    "revision": "cf270e87febe5e1ca99d",
+    "url": "/carox/static/js/main.51c16284.chunk.js"
   },
   {
     "revision": "10755e6d01a3a1236a3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carox/precache-manifest.7067e85a6ee95fc65331d4974ae016ce.js"
+  "/carox/precache-manifest.021c221d500c24d520eb4a16592fb962.js"
 );
 
 self.addEventListener('message', (event) => {
